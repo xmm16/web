57e2ea4222733683c34d9309266c4bc93e0b0aae
xmm16 <dev@fault.wtf>
2024-11-13 01:56:11 -0300

    Yap

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "SIGSEGV";
+export const SITE_DESCRIPTION =
+  "Program received signal SIGSEGV, Segmentation fault. Other than that, here I host my projects.";
