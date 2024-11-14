852599948242b0145998439b8e5da0b0b060c88b
xmm16 <dev@fault.wtf>
2024-11-14 03:19:32 -0300

    the

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
@@ -12,8 +16,26 @@ export default defineConfig({
     defaultStrategy: "viewport",
   },
   site: "https://fault.wtf",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    partytown({
+      config: { debug: false },
+    }),
+    tailwind(),
+  ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
+    shikiConfig: {
+      theme: "aurora-x",
+      wrap: true,
+    },
+  },
+  experimental: {
+    contentCollectionCache: true,
+    clientPrerender: true,
+    contentIntellisense: true,
+    contentLayer: true,
   },
 });
+
